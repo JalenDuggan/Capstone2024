f0f8ce1f22822462bf85fca521943a2f114b5550
Jalen Duggan <jalen.j.duggan@gmail.com>
2023-11-05 09:51:24 -0500

    add dotenv package to enable environment variables

--- backend/src/config/pinecone.ts
+++ backend/src/config/pinecone.ts
@@ -1,6 +1,9 @@
 /*
   Change the namespace to the requied format of pinecone
  */
+import dotenv from "dotenv";
+
+dotenv.config();
 
 if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error('Missing Pinecone index name in .env file');
