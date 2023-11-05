b17cfbf14324f38ab1caf7536be253dd4bf908c2
Jalen Duggan <jalen.j.duggan@gmail.com>
2023-11-05 01:55:39 -0400

    set up config files to handle environment variable so that the pinecone variable names match correctly

---
+++ backend/src/config/pinecone.ts
@@ -0,0 +1,11 @@
+/*
+  Change the namespace to the requied format of pinecone
+ */
+
+if (!process.env.PINECONE_INDEX_NAME) {
+  throw new Error('Missing Pinecone index name in .env file');
+}
+
+const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
+
+export { PINECONE_INDEX_NAME };
